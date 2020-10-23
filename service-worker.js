2a15efb5491347bef104663843d2226edd87b03d
Nova+ <cszonalt@gmail.com>
2020-10-23 19:17:53 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe0d33ba4ac30cac1268fb0244a37a48.js"
+  "/precache-manifest.d06a20faa95c744e5056c1e208b71f3f.js"
 );
 
 self.addEventListener('message', (event) => {
